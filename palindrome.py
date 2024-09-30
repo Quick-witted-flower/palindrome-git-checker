15b4da3f9a0795c94d82775723af7a5acda0d493
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-30 23:11:27 +0200

    Add palindrome function with documentation

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(word):
+    """
+    Sprawdza, czy dany wyraz jest palindromem.
+    
+    Argument:
+    word -- ciąg znaków (str), który chcemy sprawdzić
+    
+    Zwraca:
+    True -- jeśli wyraz jest palindromem
+    False -- jeśli wyraz nie jest palindromem
+    """
